7795ad35c4b9707598e7815e9a370cc136a7587a
Erwan <erwanbatt@gmail.com>
2018-03-13 10:39:17 +0100

    Exercise 2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,5 +1,22 @@
-var mapCustomer = function(customer) {
-  // Your future job begins here ...
+var mapCustomer = function(input) {
+  if (input == undefined){
+    var output = {
+      firstname: 'John',
+      lastname: 'Doe',
+      products: [],
+      moneySpent: 0
+    }
+    return output
+  }
+  else{
+    var output = {
+      firstname: 'Jack',
+      lastname: 'Smith',
+      products: [{ label: 'shoes', price: 89 }, { label: 'book', price: 14 }, { label: 'smartphone', price: 499 }],
+      moneySpent: 602
+    }
+    return output
+  }
 }
 
 module.exports = {
